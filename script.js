c20cb1d569f06301691046a9e0ec07dff3225e12
sire <uchepibe@gmail.com>
2022-10-18 18:54:12 -0700

    added nav menu

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById('menu__btn')
+const navMenu = document.getElementById('menu')
 
 btn.addEventListener('click', () =>{
     btn.classList.toggle('open')
+    navMenu.classList.toggle('closed__menu')
 }) 
